a7d6e90d22b6439fa518c00c24031f6cd74efb6a
≀Paulo Portela <49965144+aghuttun@users.noreply.github.com>
2025-07-25 23:51:51 +0100

    Update uipath.py

--- uipathlib/uipath.py
+++ uipathlib/uipath.py
@@ -14,6 +14,7 @@ from typing import Any, Type
 # Creates a logger for this module
 logger = logging.getLogger(__name__)
 
+
 class UiPath(object):
     @dataclasses.dataclass
     class Configuration:
